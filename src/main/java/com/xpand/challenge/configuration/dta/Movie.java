8a966838644b5068639c74deaaa13a1f49f408fd
Ricardo Morais <rfcm29@gmail.com>
2024-12-15 14:51:29 +0000

    Create entity and add connection to H2 database. Columns are created based on Entity

---
+++ src/main/java/com/xpand/challenge/configuration/dta/Movie.java
@@ -0,0 +1,21 @@
+package com.xpand.challenge.configuration.dta;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public @Data class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private Date date;
+    private int rank;
+    private long revenue;
+}

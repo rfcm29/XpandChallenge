fe3ca7b6303041c1a6ebee4db41b130f0ba3a64d
Ricardo Morais <rfcm29@gmail.com>
2024-12-15 14:33:24 +0000

    Inicial commit

---
+++ src/test/java/com/xpand/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpand.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
